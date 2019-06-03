b2d2f3b0e333638cb7ff714f7fb0bccaaa46b42d
DIVYA BAJAJ <divyabajaj0025@gmail.com>
2019-06-03 09:02:40 -0700

    Create Order.hs

---
+++ Order.hs
@@ -0,0 +1,7 @@
+module Order where
+import Views
+--Template to define order
+
+--type Order = Expr -> [Expr] --Redex to be choosen from an expression is given as output
+orderRedex :: Order
+orderRedex = undefined
